946871c303ed50e10ab66fc4ff5ebe8c49c0d0a6
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-02-17 14:43:33 +0100

    sidenav working

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import App from "./App.svelte";
+import SideNav from "./SideNav.svelte";
+
+var sidebar = new SideNAv({
+  target: document.querySelector(".sidebar"),
+});
 
 var app = new App({
   target: document.body,
 });
 
 export default app;
+export default sedebar;
